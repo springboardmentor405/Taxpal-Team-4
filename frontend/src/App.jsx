cb0031b43c30e62a2ebb712d39ec3caeb7ea687e
Sonha P <sonap03022005@gmail.com>
2026-02-19 15:00:51 +0530

    deleted the file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./Login";
 import Register from "./Registration";
-import ForgotPassword from "./ForgotPassword";
+
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
