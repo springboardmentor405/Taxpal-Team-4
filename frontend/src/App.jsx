0c739b369ebcb9a6e0b68571d1f9012f85ab7914
Sonha P <sonap03022005@gmail.com>
2026-02-20 15:38:50 +0530

    changed the import file name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import Register from "./Register";
+import Register from "./Registration";
 import ForgotPassword from "./ForgotPassword";
 import VerifyCode from "./VerifyCode";
 import ResetPassword from "./ResetPassword";
