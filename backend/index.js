9dbabffa9127fa0c6287828806222ddf0a9af228
Urvashi1407 <baraiyaurvashi027@gmail.com>
2026-02-25 14:03:36 +0530

    Added backend models, index.js, and connected login/registration pages with MongoDB Compass

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/User");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb://localhost:27017/Taxpal")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("success");
+      } else {
+        res.json("password is incorrect");
+      }
+    } else {
+      res.json("No record existed");
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  UserModel.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("server is running on port 3001");
+});
