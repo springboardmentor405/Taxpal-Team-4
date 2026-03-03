700090a341ba91f951944a64bb1e85a8489a901f
Urvashi1407 <baraiyaurvashi027@gmail.com>
2026-03-03 19:04:53 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const UserModel = require("./models/User");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
@@ -11,26 +12,48 @@ mongoose
   .connect("mongodb://localhost:27017/Taxpal")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
-
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  UserModel.findOne({ email: email }).then((user) => {
-    if (user) {
-      if (user.password === password) {
-        res.json("success");
-      } else {
-        res.json("password is incorrect");
-      }
-    } else {
-      res.json("No record existed");
-    }
-  });
+
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return res.json("No record existed");
+  }
+
+  // compare hashed password
+  const match = await bcrypt.compare(password, user.password);
+
+  if (match) {
+    res.json("success");
+  } else {
+    res.json("password is incorrect");
+  }
 });
 
-app.post("/register", (req, res) => {
-  UserModel.create(req.body)
-    .then((user) => res.json(user))
-    .catch((err) => res.json(err));
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check if user already exists
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.json("User already exists");
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await UserModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.json("Account created");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 app.listen(3001, () => {
